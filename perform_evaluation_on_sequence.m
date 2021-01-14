875e1a5582316579efc34cdb7f2104054300cac3
Borja <borja.bovcon@gmail.com>
2021-01-14 21:59:19 +0100

    Update perform_evaluation_on_sequence.m
    fixed masks

--- perform_evaluation_on_sequence.m
+++ perform_evaluation_on_sequence.m
@@ -17,25 +17,26 @@ function [output_results_cell, output_detections] = perform_evaluation_on_sequen
     %% Read masks of USV parts
     % Read mask that filters out the areas of the USV visible in the image
     if(seq.id >= 1 && seq.id <= 9)
-        boat_parts_mask = imread(fullfile(paths.USV_parts_masks, 'plovba_1_L.png'));
-        black_areas = imread(fullfile(paths.dataset_path, 'masks_rectification_black_area', 'plovba01.png'));
+        boat_parts_mask_l = imread(fullfile(paths.USV_parts_masks, 'kope67_L.png'));
+        boat_parts_mask_r = imread(fullfile(paths.USV_parts_masks, 'kope67_R.png'));
+        
     elseif(seq.id >= 10 && seq.id <= 14)
-        boat_parts_mask = imread(fullfile(paths.USV_parts_masks, 'plovba_2_L.png'));
-        black_areas = imread(fullfile(paths.dataset_path, 'masks_rectification_black_area', 'plovba02.png'));
+        boat_parts_mask_l = imread(fullfile(paths.USV_parts_masks, 'kope71_L.png'));
+        boat_parts_mask_r = imread(fullfile(paths.USV_parts_masks, 'kope71_R.png'));
+
     elseif(seq.id >= 15 && seq.id <= 18)
-        boat_parts_mask = imread(fullfile(paths.USV_parts_masks, 'plovba_3_L.png'));
-        black_areas = imread(fullfile(paths.dataset_path, 'masks_rectification_black_area', 'plovba03.png'));
+        boat_parts_mask_l = imread(fullfile(paths.USV_parts_masks, 'kope75_L.png'));
+        boat_parts_mask_r = imread(fullfile(paths.USV_parts_masks, 'kope75_R.png'));
+
     elseif(seq.id >= 19 && seq.id <= 26)
-        boat_parts_mask = imread(fullfile(paths.USV_parts_masks, 'plovba_4_L.png'));
-        black_areas = imread(fullfile(paths.dataset_path, 'masks_rectification_black_area', 'plovba04.png'));
+        boat_parts_mask_l = imread(fullfile(paths.USV_parts_masks, 'kope81_L.png'));
+        boat_parts_mask_r = imread(fullfile(paths.USV_parts_masks, 'kope81_R.png'));
+        
     elseif(seq.id >= 27 && seq.id <= 28)
-        boat_parts_mask = imread(fullfile(paths.USV_parts_masks, 'plovba_5_L.png'));
-        black_areas = imread(fullfile(paths.dataset_path, 'masks_rectification_black_area', 'plovba05.png'));
+        boat_parts_mask_l = imread(fullfile(paths.USV_parts_masks, 'kope82_L.png'));
+        boat_parts_mask_r = imread(fullfile(paths.USV_parts_masks, 'kope82_R.png'));
+
     end
-    
-    black_areas = imresize(black_areas, [384, 512], 'method', 'nearest');
-    se = strel('disk', 10);
-    black_areas = imbinarize(imdilate(uint8(black_areas), se));
     
     %% Loop through the images....
     % Get the number of total frames in the sequence
@@ -65,12 +66,15 @@ function [output_results_cell, output_detections] = perform_evaluation_on_sequen
         
         % Override possible detections that are a couse of usv parts
         [msk_size_y, msk_size_x, ~] = size(msk);
-        boat_parts_mask_resized = imresize(boat_parts_mask, [msk_size_y, msk_size_x], 'method', 'nearest');
+        
+        boat_parts_mask_resized_l = imresize(boat_parts_mask_l, [msk_size_y, msk_size_x], 'method', 'nearest');
+        boat_parts_mask_resized_r = imresize(boat_parts_mask_r, [msk_size_y, msk_size_x], 'method', 'nearest');
+        
         for rgb_counter = 1 : 3 % loop through color channels
             % extract current color channel
             tmp = msk(:, :, rgb_counter);
             % change with the corresponding value of the water component
-            tmp(boat_parts_mask_resized == 1) = eval_params.labels(3, rgb_counter);
+            tmp(boat_parts_mask_resized_l == 1) = eval_params.labels(3, rgb_counter);
             % update the segmentation mask
             msk(:, :, rgb_counter) = tmp;
         end
@@ -97,6 +101,7 @@ function [output_results_cell, output_detections] = perform_evaluation_on_sequen
             msk = imresize(cv.remap(imresize(msk, eval_params.img_size, 'Method', 'nearest'), map_L1, map_L2), [msk_size_y, msk_size_x], 'Method', 'nearest');
         end
         
+        %{
         if(eval_params.rectified == 1)
             % remove black areas
             for rgb_counter = 1 : 3
@@ -105,6 +110,7 @@ function [output_results_cell, output_detections] = perform_evaluation_on_sequen
                 msk(:, :, rgb_counter) = tmp;
             end
         end
+        %}
 
         
         %% Postprocess output segmentation images
